c9a77aecff7b88d51e5b3a24f1ad60fd879bad66
sitrucp <sitrucp@users.noreply.github.com>
2019-03-23 14:48:00 +0100

    Update cubatweets.py

--- cubatweets.py
+++ cubatweets.py
@@ -1,23 +1,46 @@
 import tweepy
-#import MySQLdb
+import MySQLdb
 from datetime import datetime
 import random
 import sys, os
 import json
 from dateutil import tz
 from tweet_auth import * #this is another .py file with the twitter api credentials
+from db_cubatweet import db_cubatweet * #this is another .py file with the cubatweet db credentials
+
+#connect to db
+db = MySQLdb.connect(
+    host = db_cubatweet['host'], 
+    db = db_cubatweet['dbname'], 
+    user = db_cubatweet['user'], 
+    passwd = db_cubatweet['password']
+    )
+
+# Create a db cursor object to execute queries
+cur = db.cursor()
 
 #get twitter auth
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+#auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-def main():
-	with open('cubatweets.csv', 'wb') as file:
-		for status in tweepy.Cursor(api.user_timeline, id='cubagoodingjr').items(1800):
-			file.write(str(status.created_at.replace(tzinfo=tz.gettz('UTC')).astimezone(tz.gettz('America/Los_Angeles')).replace(tzinfo=None))  + ', ' + str(status.created_at.date()) + ', ' + status.text.encode('utf8') + '\n')
+users = ['cubagoodingjr']
+date_zero = datetime.strptime('2016-02-11 02:35:00', '%Y-%m-%d %H:%M:%S')
 
-if __name__ == '__main__':
-	main()
+date_now = datetime.now()
+hours = 9999.99
+days = round(((date_now-date_zero).total_seconds() /3600)/24,2)
+
+for user in users:
+    user_data = api.get_user(user)
+    print user_data.followers_count	
+    cur.execute("INSERT INTO followers(user, follower_count, date, days, hours) VALUES (%s, %s, %s, %s, %s)",
+        (user, user_data.followers_count, date_now, days, hours))
+    db.commit()
+    
+print datetime.now(), 'cubatweet'
+    
+cur.close()
+db.close()
 
     
